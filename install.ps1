4cab77755a4e5512ee9e3a2a85a7486f65e6943f
Dylan McDougall <dylanalexandermcdougall@gmail.com>
2022-08-27 22:27:56 -0400

    Update repo name

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest -OutFile .\release-posix-seh.7z -URI "https://github.com/dmcdo/mingw-tmp/raw/master/x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z"
+Invoke-WebRequest -OutFile .\release-posix-seh.7z -URI "https://github.com/dmcdo/mingw-w64-install-script/raw/master/x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z"
 Invoke-WebRequest -OutFile .\7zr.exe -URI "https://www.7-zip.org/a/7zr.exe"
 cmd /c "7zr.exe x release-posix-seh.7z -o$env:USERPROFILE\release-posix-seh -y"
 Remove-Item .\release-posix-seh.7z
@@ -7,5 +7,5 @@ cmd /c "mklink /H $env:USERPROFILE\release-posix-seh\mingw64\bin\make.exe $env:U
 $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment').path
 $newpath = "$env:USERPROFILE\release-posix-seh\mingw64\bin;" + $oldpath
 Set-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment' -Name PATH -Value $newpath
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-echo "Done."
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+Write-Output "Done."
