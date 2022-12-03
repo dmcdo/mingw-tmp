293440c399d31b062e0322b354fc9e4a555cc31a
Dylan McDougall <40744272+dmcdo@users.noreply.github.com>
2022-12-03 15:22:39 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 Invoke-WebRequest -OutFile $env:TEMP\release-posix-seh.7z -URI "https://github.com/dmcdo/mingw-w64-install-script/raw/master/x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z"
 Invoke-WebRequest -OutFile $env:TEMP\7zr.exe -URI "https://www.7-zip.org/a/7zr.exe"
-cmd /c "7zr.exe x $env:TEMP\release-posix-seh.7z -o$env:USERPROFILE\release-posix-seh -y"
+cmd /c "`"$env:TEMP\7zr.exe`" x $env:TEMP\release-posix-seh.7z -o$env:USERPROFILE\release-posix-seh -y"
 Remove-Item $env:TEMP\release-posix-seh.7z
 Remove-Item $env:TEMP\7zr.exe
 cmd /c "mklink /H $env:USERPROFILE\release-posix-seh\mingw64\bin\make.exe $env:USERPROFILE\release-posix-seh\mingw64\bin\mingw32-make.exe"
