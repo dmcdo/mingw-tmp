b917c298635048fe59bce9bf4af03fcfb6d72386
Dylan McDougall <dylanalexandermcdougall@outlook.com>
2023-05-31 09:37:19 -0400

    Switch from POSIX to Win32

--- install.ps1
+++ install.ps1
@@ -1,10 +1,10 @@
-Invoke-WebRequest -OutFile $env:TEMP\release-posix-seh.7z -URI "https://github.com/dmcdo/mingw-w64-install-script/raw/master/x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z"
+Invoke-WebRequest -OutFile $env:TEMP\release-win32-seh.7z -URI "https://github.com/dmcdo/mingw-w64-install-script/raw/master/x86_64-8.1.0-release-win32-seh-rt_v6-rev0.7z"
 Invoke-WebRequest -OutFile $env:TEMP\7zr.exe -URI "https://www.7-zip.org/a/7zr.exe"
-cmd /c "`"$env:TEMP\7zr.exe`" x $env:TEMP\release-posix-seh.7z -o$env:USERPROFILE\release-posix-seh -y"
-Remove-Item $env:TEMP\release-posix-seh.7z
+cmd /c "`"$env:TEMP\7zr.exe`" x $env:TEMP\release-win32-seh.7z -o$env:USERPROFILE\release-win32-seh -y"
+Remove-Item $env:TEMP\release-win32-seh.7z
 Remove-Item $env:TEMP\7zr.exe
-cmd /c "mklink /H $env:USERPROFILE\release-posix-seh\mingw64\bin\make.exe $env:USERPROFILE\release-posix-seh\mingw64\bin\mingw32-make.exe"
+cmd /c "mklink /H $env:USERPROFILE\release-win32-seh\mingw64\bin\make.exe $env:USERPROFILE\release-win32-seh\mingw64\bin\mingw32-make.exe"
 $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment').path
-$newpath = "$env:USERPROFILE\release-posix-seh\mingw64\bin;" + $oldpath
+$newpath = "$env:USERPROFILE\release-win32-seh\mingw64\bin;" + $oldpath
 Set-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment' -Name PATH -Value $newpath
 Write-Output "Done. Log out and log back in for the changes to take full effect."
